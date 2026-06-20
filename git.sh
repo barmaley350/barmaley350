bc653c1321e75b9685c177ae59453d30ee9175df
Andrey Frolov <darmaley40@yandex.ru>
2026-06-20 10:19:23 +0300

    chore: add project configuration and initial files

---
+++ git.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+read -r -d '' PROMPT <<'EOF'
+Создай commit message по Conventional Commits на основе git diff.
+Используй тип feat/fix/refactor/docs/style/test/chore.
+Формат: "<тип>: <одно предложение до 50 символов без markdown и пояснений>".
+EOF
+# read -r -d '' PROMPT <<'EOF'
+# Создай commit message по Conventional Commits на основе git diff.
+# Используй тип feat/fix/refactor/docs/style/test/chore.
+# Формат заголовка: "<тип>: <одно предложение до 50 символов без markdown и пояснений>"
+# Заголовой напиши на английском языке.
+# После заголовка оставь пустую строку и добавь тело коммита: кратко опиши на русском языке какие именно изменения были сделаны (не более 3–4 строк), зачем это нужно и на что может повлиять. Каждую строку в описании начинай с новой строки. Не использу markdown.
+# EOF
+
+uv run python3 main.py
+git add .
+diff_output=$(git diff --cached)
+
+if [[ -z "$diff_output" ]]; then
+    echo "Нет изменений в индексе"
+else
+    {
+        printf '%s\n\n' "$PROMPT"
+        printf '%s\n' "$diff_output"
+    } | xclip -selection clipboard
+
+    echo "Diff и промпт скопированы в буфер обмена"
+fi
+
+read -p "Введите commit: " commit
+
+git commit -m "$commit"
+git push origin main
---
+++ main.py
@@ -0,0 +1,104 @@
+"""main."""
+
+from pathlib import Path
+
+
+def create_steck_links(steck_md: Path, output_md: Path) -> None:
+    """Create links.
+
+    :param steck_md: _description_
+    :type steck_md: Path
+    :param output_md: _description_
+    :type output_md: Path
+
+    For icons
+    https://simpleicons.org/
+
+    For badges
+    https://shields.io/badges/static-badge
+    """
+    stack = {
+        "python": {
+            "name": "Python",
+            "logo": "python",
+        },
+        "pandas": {
+            "name": "Pandas",
+            "logo": "pandas",
+        },
+        "fastapi": {
+            "name": "FastAPI",
+            "logo": "fastapi",
+        },
+        "docker": {
+            "name": "Docker",
+            "logo": "docker",
+        },
+        "postgresql": {
+            "name": "PostgreSQL",
+            "logo": "postgresql",
+        },
+        "numpy": {
+            "name": "NumPy",
+            "logo": "numpy",
+        },
+        "clickhouse": {
+            "name": "ClickHouse",
+            "logo": "clickhouse",
+        },
+        "sqlalchemy": {
+            "name": "SQLAlchemy",
+            "logo": "sqlalchemy",
+        },
+        "pytest": {
+            "name": "Pytest",
+            "logo": "pytest",
+        },
+        "pydantic": {"name": "Pydantic", "logo": "pydantic"},
+        "Apache-Airflow": {"name": "Apache_Airflow", "logo": "apache-airflow"},
+        "Apache-Superset": {"name": "Apache_Superset", "logo": "apache-superset"},
+        "django": {"name": "Django", "logo": "django"},
+        "Nuxt": {"name": "NuxtJS", "logo": "nuxt"},
+        "Git": {"name": "Git", "logo": "git"},
+        "Celery": {"name": "Celery", "logo": "celery"},
+    }
+    with steck_md.open("r", encoding="utf-8") as input_file:
+        content = input_file.read()
+
+    for item in stack:
+        name = stack[item]["name"]
+        logo = stack[item]["logo"]
+        url = f"![Static Badge](https://img.shields.io/badge/{name}-abcdef?style=for-the-badge&logo={logo})"
+        content += f"\n{url}"
+
+    with output_md.open("a", encoding="utf-8") as output_file:
+        output_file.write(f"{content} \n\n")
+
+
+def create_empty_md(output_md: Path):
+    output_md.write_text("", encoding="utf-8")
+
+
+def append_md(file_path: Path, output_md: Path):
+    with file_path.open("r", encoding="utf-8") as input_file:
+        content = input_file.read()
+
+    with output_md.open("a", encoding="utf-8") as output_file:
+        output_file.write(f"{content} \n\n")
+
+
+if __name__ == "__main__":
+    resources = {
+        # "contact": Path(__file__).parent / "md/CONTACTS.md",
+        "about": Path(__file__).parent / "md/ABOUT.md",
+        "steck": Path(__file__).parent / "md/STECK.md",
+    }
+    output_md = Path(__file__).parent / "README.md"
+
+    create_empty_md(output_md)
+
+    for key, file_path in resources.items():
+        if key == "steck":
+            create_steck_links(file_path, output_md)
+        else:
+            append_md(file_path, output_md)
